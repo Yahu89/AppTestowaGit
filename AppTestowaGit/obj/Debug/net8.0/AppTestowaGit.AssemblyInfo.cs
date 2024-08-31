bebb7448bfd89b66a97885a8c6010d2b9411cd79
Yahu89 <krzysztof.wohlert@gmail.com>
2024-08-31 16:49:47 +0200

    Human added

--- AppTestowaGit/obj/Debug/net8.0/AppTestowaGit.AssemblyInfo.cs
+++ AppTestowaGit/obj/Debug/net8.0/AppTestowaGit.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AppTestowaGit")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5fdac42b15a674315059059c99a9b13d1f257a1d")]
 [assembly: System.Reflection.AssemblyProductAttribute("AppTestowaGit")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AppTestowaGit")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
